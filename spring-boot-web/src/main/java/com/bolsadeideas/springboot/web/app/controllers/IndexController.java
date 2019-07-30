f1efbf29a608142363e220fe19f2044082d1ead6
jrVillegas <javier.villegasgo@gmail.com>
2019-07-30 16:28:23 +0200

    adding models

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,15 +3,33 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 
 @Controller
+@RequestMapping("app")
 public class IndexController {
 
-	@GetMapping({"/index", "/", "/home"})
+	@GetMapping({"/index", "/","", "/home"})
 	public String index(Model model) {
 		model.addAttribute("titulo", "hola Spring framework!");
 		
 		return "index";	
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		
+		usuario.setNombre("Javier");
+		usuario.setApellido("Villegas");
+		usuario.setEmail("javier@correo.com");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario ".concat(usuario.getNombre()));
+		
+		return "perfil";
 	}
 	
 }
