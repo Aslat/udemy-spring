f10cd818fa51b9015ff81c8120e349ad730326cf
jrVillegas <javier.villegasgo@gmail.com>
2019-08-01 13:18:59 +0200

    recivir y enviar datos desde la URL

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,8 +1,13 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
@@ -31,5 +36,26 @@ public class IndexController {
 		
 		return "perfil";
 	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+
+
+		
+		model.addAttribute("titulo", "Listado de usuarios");
+		
+		return "listar";
+	}
+	
+	//Sirve para pasar atributos a todas las vistas
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Javier", "Villegas", "javier.villegas@mail.com"), 
+				new Usuario("John", "Doe", "john.doe@mail.com"),
+				new Usuario("Jane", "Done", "jane.doe@mail.com"));
+	
+		return usuarios;
+	}
+	
 	
 }
