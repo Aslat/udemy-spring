b028bcb03320895e1bd7fc0712053b0a5bd8f0a2
Javier <javier@MacBook-Pro-de-Javier.local>
2019-08-04 09:04:53 +0200

    añadiendo recursos estaticos

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,18 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @RequestMapping("app")
 public class IndexController {
 
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String 	textoIndex;
+	
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String 	textoPerfil;
+	
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String 	textoListar;
+	
 	@GetMapping({"/index", "/","", "/home"})
 	public String index(Model model) {
-		model.addAttribute("titulo", "hola Spring framework!");
+		model.addAttribute("titulo", textoIndex);
 		
 		return "index";	
 	}
@@ -32,7 +42,7 @@ public class IndexController {
 		usuario.setEmail("javier@correo.com");
 		
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil del usuario ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 		
 		return "perfil";
 	}
@@ -42,7 +52,7 @@ public class IndexController {
 
 
 		
-		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("titulo", textoListar);
 		
 		return "listar";
 	}
