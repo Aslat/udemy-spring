321f658dca59dfd3312b6c1c9efba15ea7d89f24
jrVillegas <javier.villegasgo@gmail.com>
2019-08-14 16:34:36 +0200

    new project to implement dependency injection

---
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -0,0 +1,37 @@
+package com.bolsadeideas.springboot.di.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bolsadeideas.springboot.di.app.models.service.IServicio;
+
+@Controller
+public class IndexController {
+	
+	@Autowired
+	private IServicio servicio;
+	
+	@GetMapping({"/", "", "/index"})
+	public String index(Model model) {
+		model.addAttribute("objeto", servicio.operacion());
+		
+		return "index";
+	}
+
+	//El constructor lo inyecta automaticamente, no hace falta el autowired
+	//@Autowired
+//	public IndexController(IServicio servicio) {
+//		super();
+//		this.servicio = servicio;
+//	}
+
+//	@Autowired
+//	public void setServicio(IServicio servicio) {
+//		this.servicio = servicio;
+//	}
+	
+	
+	
+}
