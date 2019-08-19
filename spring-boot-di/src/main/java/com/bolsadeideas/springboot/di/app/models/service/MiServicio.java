125f584f8158934c2cf229dad113fc0bed386b00
jrVillegas <javier.villegasgo@gmail.com>
2019-08-19 16:27:32 +0200

    empezando con la factura

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -1,8 +1,10 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component("miServicioSimple")
+//@Component("miServicioSimple")
+//@Primary
 public class MiServicio implements IServicio{
 	
 	@Override
